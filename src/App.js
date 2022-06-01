3165ffb2df3a5b28370e2de4489f8c314c930867
meetpokiya <pokiyameet0@gmail.com>
2022-06-01 09:52:59 +0530

    01-06

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import MiniDrawer from './Component/Layout';
+import { Switch,Route  } from 'react-router-dom';
+import Medicine from './Container/Medicine';
+import Doctor from './Container/Doctor';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MiniDrawer>
+      <Switch>
+        <Route exact path={'/Medicine'} component={Medicine}/>
+        <Route exact path={'/Doctor'} component={Doctor}/>
+      </Switch>
+    </MiniDrawer>
   );
 }
 
---
+++ src/Container/Medicine.js
@@ -0,0 +1,98 @@
+import React, { useState } from 'react'
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+export default function Medicine() {
+    const [open, setOpen] = React.useState(false);
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    const [quantity, setQuantity] = useState('');
+    const [expiry, setExpiry] = useState('');
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handSubmit = () => {
+      let data = {
+          
+        name,
+        price,
+        quantity,
+        expiry
+      }
+
+      // console.log(data);
+
+      localStorage.setItem('Medicine', JSON.stringify(data))
+      
+
+      setOpen(false);
+    }
+
+    return (
+        <div>
+        <Button variant="outlined" onClick={handleClickOpen}>
+            Add Medicine
+        </Button>
+        <Dialog open={open} onClose={handleClose}>
+          <DialogTitle>Add Medicine</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              name='name'
+              label="Medicine name"
+              fullWidth
+              variant="standard"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="price"
+              name='price'
+              label="Medicine price"
+              fullWidth
+              variant="standard"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="quantity"
+              name='quantity'
+              label="Medicine quantity"
+              fullWidth
+              variant="standard"
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+            <TextField
+              autoFocus
+              margin="dense"
+              id="expiry"
+              name='expiry'
+              label="Medicine expiry"
+              fullWidth
+              variant="standard"
+              onChange={(e) => setExpiry(e.target.value)}
+            />     
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handSubmit}>Submit</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    )
+}
